457dd3af1c2b6d42215ff11228bb2b549f8d6bd2
jonarroh <jonarrodi99@gmail.com>
2022-06-24 12:45:57 -0500

    🛠️: entry y entry view y se ve en pantalla

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="entry-container mb-3 pointer p-2">
+	<div
+		class="entry-container mb-3 pointer p-2"
+		@click="$router.push({ name: 'Entry', params: { id: 1 } })"
+	>
 		<div class="entry-title d-flex">
 			<span class="text-success fs-5 ww-bold">15 </span>
 			<span class="mx-1 fs-5">julio </span>
