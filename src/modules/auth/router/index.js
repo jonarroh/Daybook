fd8a7d396d1a46f08516c6380996b5bd95721b7f
jonarroh <jonarrodi99@gmail.com>
2022-06-30 14:08:22 -0500

    🛠️: views login & register

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,5 +1,16 @@
 export default {
 	name: 'auth',
 	component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
-	children: []
+	children: [
+		{
+			path: '',
+			name: 'login',
+			component: () => import('@/modules/auth/views/LoginView.vue')
+		},
+		{
+			path: '/registrar',
+			name: 'registrar',
+			component: () => import('@/modules/auth/views/RegisterView.vue')
+		}
+	]
 };
