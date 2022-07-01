d4f1d29d07f184f86b1562c546d7589aa3fb4e17
jonarroh <jonarrodi99@gmail.com>
2022-07-01 10:29:56 -0500

    🛠️: new store(auth)

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 
 import daybook from '../modules/daybook/store/daybook';
+import auth from '../modules/auth/store/';
 
 export default createStore({
 	modules: {
+		auth,
 		daybook
 	}
 });
